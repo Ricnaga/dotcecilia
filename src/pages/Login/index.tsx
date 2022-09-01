6841023276224fcc5cd5b1524fa449a5f8b6894b
rinagatomy <rinagatomy@paschoalotto.com.br>
2022-09-01 11:03:09 -0300

    refactor: login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,3 +1,11 @@
+import React from 'react';
+import { LoginForm } from './LoginForm';
+
 export function LoginPage() {
-  return <h1>LoginPage</h1>;
+  return (
+    <div className="grid items-center justify-center h-full mt-20">
+      <p className="text-7xl font-medium mb-8 text-center">Fazer Login</p>
+      <LoginForm />
+    </div>
+  );
 }
