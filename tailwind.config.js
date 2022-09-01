b906c85f6aaa6c5159dacf6c095623464f14ec7a
rinagatomy <rinagatomy@paschoalotto.com.br>
2022-09-01 10:56:04 -0300

    refactor: Refactored tailwind config | update gitginore

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
+const path = require('path');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    path.join(process.cwd(), 'src', '**', '*.[j|t]s?(x)'),
+    path.join(process.cwd(), 'index.html'),
+  ],
   theme: {
+    fontFamily: {
+      sans: ['Outfit', `sans-serif`],
+      serif: ['Merriweather', 'serif'],
+    },
     extend: {},
   },
   plugins: [],
