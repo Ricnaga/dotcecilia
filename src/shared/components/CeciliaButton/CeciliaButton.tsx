58160ca5eabc8c4b32ca0fb239e88db4db952bcf
rinagatomy <rinagatomy@paschoalotto.com.br>
2022-09-02 11:24:48 -0300

    refactor: change button props and styles

--- src/shared/components/CeciliaButton/CeciliaButton.tsx
+++ src/shared/components/CeciliaButton/CeciliaButton.tsx
@@ -4,19 +4,17 @@ type CeciliaButtonButtonProps =
   React.ButtonHTMLAttributes<HTMLButtonElement> & {
     title: string;
     type?: 'submit' | 'reset' | 'button';
+    className?: string;
   };
 
 export function CeciliaButton({
   type = 'button',
   title,
+  className,
   ...props
 }: CeciliaButtonButtonProps) {
   return (
-    <button
-      className="w-full px-4 py-2 bg-yellow-300 hover:bg-yellow-400 active:bg-yellow-500 focus:outline-none focus:ring focus:ring-sky-900 rounded font-bold text-2xl"
-      type={type}
-      {...props}
-    >
+    <button className={`btn btn-yellow ${className}`} type={type} {...props}>
       {title}
     </button>
   );
