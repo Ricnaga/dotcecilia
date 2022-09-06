e5bc3b73b1082efb2d079337fb022a437fa04338
rinagatomy <rinagatomy@paschoalotto.com.br>
2022-09-06 18:28:31 -0300

    feat: Add disclosure for transport voucher field

--- src/pages/Calculator/components/CalculatorTransport/index.tsx
+++ src/pages/Calculator/components/CalculatorTransport/index.tsx
@@ -1,18 +1,49 @@
-import { CeciliaText } from '../../../../shared/components';
+import { CeciliaNumber, CeciliaText } from '../../../../shared/components';
 import { CalculatorDisclosure } from '../CalculatorDisclosure/CalculatorDisclosure';
 
-export function CalculatorTransport() {
+export type CalculatorTransportData = {
+  transportValue: number;
+  firstBusinessDay: Date;
+  discountedDays: number;
+};
+
+type CalculatorTransportProps = {
+  onChangeTransport: (
+    key: keyof CalculatorTransportData,
+    value: number,
+  ) => void;
+  transport: CalculatorTransportData;
+};
+
+export function CalculatorTransport({
+  onChangeTransport,
+  transport,
+}: CalculatorTransportProps) {
   return (
     <CalculatorDisclosure title="Vale transporte">
       <div className="accordion-content">
         <div className="col-span-4">
-          <CeciliaText placeholder="R$" label="Valor" />
+          <CeciliaNumber
+            placeholder="R$"
+            label="Valor"
+            value={transport.transportValue}
+            onChange={(e) =>
+              onChangeTransport('transportValue', e.target.valueAsNumber)
+            }
+          />
         </div>
         <div className="col-span-4">
           <CeciliaText placeholder="dd/mm/aaaa" label="1º dia útil do mês" />
         </div>
         <div className="col-span-4">
-          <CeciliaText placeholder="Dias" label="Dias descontados" />
+          <CeciliaNumber
+            placeholder="Dias"
+            label="Dias descontados"
+            value={transport.discountedDays}
+            onChange={(e) =>
+              onChangeTransport('discountedDays', e.target.valueAsNumber)
+            }
+          />
         </div>
       </div>
     </CalculatorDisclosure>
