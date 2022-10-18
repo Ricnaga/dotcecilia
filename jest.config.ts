2910c52c49d722cb5f37788332bbeaa8f3a5fbaf
rinagatomy <rinagatomy@paschoalotto.com.br>
2022-10-18 10:20:12 -0300

    test: initial config

--- jest.config.ts
+++ jest.config.ts
@@ -1,14 +1,23 @@
-import type { Config } from '@jest/types';
+import { pathsToModuleNameMapper, JestConfigWithTsJest } from 'ts-jest';
+import { compilerOptions } from './tsconfig.json';
 
-const config: Config.InitialOptions = {
+const config: JestConfigWithTsJest = {
   clearMocks: true,
   collectCoverageFrom: [
     '<rootDir>/src/pages/**/*.[jt]s?(x)',
     '!<rootDir>/src/pages/**/*.{spec,test}.[jt]s?(x)',
+    '<rootDir>/src/shared/components/**/*.[jt]s?(x)',
+    '!<rootDir>/src/shared/components/**/*.{spec,test}.[jt]s?(x)',
+    '<rootDir>/src/shared/hooks/*.[jt]s',
+    '!<rootDir>/src/shared/hooks/*.{spec,test}.[jt]s',
   ],
+  preset: 'ts-jest',
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageReporters: ['lcov', 'json', 'text', 'text-summary'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
   roots: ['<rootDir>/src'],
   setupFilesAfterEnv: ['<rootDir>/src/application/test/setup.ts'],
   testEnvironment: 'jsdom',
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import EnvironmentPlugin from 'vite-plugin-environment';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({ fastRefresh: true }), tsconfigPaths()],
+  plugins: [
+    react({ fastRefresh: true }),
+    tsconfigPaths(),
+    EnvironmentPlugin('all'),
+  ],
   server: {
     port: 3000,
     hmr: true,
