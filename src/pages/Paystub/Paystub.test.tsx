6892062bd4bd5b95b6999a8a6887fdea2f7f0aeb
rinagatomy <rinagatomy@paschoalotto.com.br>
2022-10-21 16:21:32 -0300

    refactor: Refactor old tests

--- src/pages/Paystub/Paystub.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import { PaystubPage } from './Paystub';
-
-describe('Page: PaystubPage', () => {
-  it('should render correctly', () => {
-    const { container } = render(<PaystubPage />);
-    expect(container).toMatchSnapshot();
-  });
-});
