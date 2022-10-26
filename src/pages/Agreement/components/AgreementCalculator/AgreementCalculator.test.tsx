b78e9bc71f8ec07fc6ac17e1ec9ac3a4276ddc79
rinagatomy <rinagatomy@paschoalotto.com.br>
2022-10-26 13:00:59 -0300

    refactor: refactor tests

--- src/pages/Agreement/components/AgreementCalculator/AgreementCalculator.test.tsx
+++ src/pages/Agreement/components/AgreementCalculator/AgreementCalculator.test.tsx
@@ -40,45 +40,41 @@ describe('Components: AdvancePaymentCalculator', () => {
     expect(mockValues.name).toEqual(value);
   });
 
-  it('should be able to type a salary', () => {
-    const value = 2000;
-    fireEvent.change(screen.getByPlaceholderText(/salário/i), {
-      target: { value },
-    });
-    expect(mockValues.salary).toEqual(value);
+  it('should test input number', () => {
+    const numberInputs = [
+      {
+        placeholder: /salário/i,
+        value: 2000,
+      },
+      {
+        placeholder: /descontos \(r\$\)/i,
+        value: 2000,
+      },
+    ];
+
+    numberInputs.map((input) =>
+      fireEvent.change(screen.getByPlaceholderText(input.placeholder), {
+        target: { value: input.value },
+      }),
+    );
+    expect(mockValues.salary).toEqual(numberInputs[0].value);
+    expect(mockValues.discount).toEqual(numberInputs[1].value);
   });
 
-  it('should be able to type a discount', () => {
-    const value = 2000;
-    fireEvent.change(screen.getByPlaceholderText(/descontos \(r\$\)/i), {
-      target: { value },
-    });
-    expect(mockValues.discount).toEqual(value);
-  });
-
-  it('should be able to type a initial Date', () => {
+  it('should test input Date', () => {
     const value = new Date(2020, 5, 1);
 
-    const initialDateInput = within(
-      screen.getByText(/data inicial/i),
-    ).getByDisplayValue(/2022-11-10/i);
+    [/data inicial/i, /data final/i].map((date) => {
+      const dateInput = within(screen.getByText(date)).getByDisplayValue(
+        /2022-11-10/i,
+      );
 
-    fireEvent.change(initialDateInput, {
-      target: { valueAsDate: value },
+      return fireEvent.change(dateInput, {
+        target: { valueAsDate: value },
+      });
     });
+
     expect(new Date(mockValues.startDate)).toEqual(value);
-  });
-
-  it('should be able to type a end Date', () => {
-    const value = new Date(2020, 5, 1);
-
-    const initialDateInput = within(
-      screen.getByText(/data final/i),
-    ).getByDisplayValue(/2022-11-10/i);
-
-    fireEvent.change(initialDateInput, {
-      target: { valueAsDate: value },
-    });
-    expect(new Date(mockValues.startDate)).toEqual(value);
+    expect(new Date(mockValues.endDate)).toEqual(value);
   });
 });
