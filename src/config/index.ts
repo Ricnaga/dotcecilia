fc691ba79d9899b3fa3ecf8cea602e3e7be2dc23
Ricnaga <ric.nagato@gmail.com>
2023-09-22 19:26:33 -0300

    chore(envinroment): Rename environment variables

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,5 +2,7 @@ export const ENV_USER_STORAGE_KEY = String(process.env.VITE_USER_STORAGE_KEY);
 export const ENV_USER = String(process.env.VITE_USER);
 export const ENV_PASSWORD = String(process.env.VITE_PASSWORD);
 export const ENV_COMPANY_NAME = String(process.env.VITE_COMPANY_NAME);
-export const ENV_COMPANY_ADDRESS = String(process.env.VITE_COMPANY_ADDRESS);
-export const ENV_COMPANY_CPNJ = String(process.env.VITE_COMPANY_CPNJ);
+export const ENV_COMPANY_ADDRESS = String(
+  process.env.VITE_USER_COMPANY_ADDRESS,
+);
+export const ENV_COMPANY_CPNJ = String(process.env.VITE_USER_COMPANY_CPNJ);
