8b0a8d06bfa2492bf864bffc4e2588f51fdc2b95
Ricnaga <ric.nagato@gmail.com>
2023-09-23 11:10:48 -0300

    fix(switch): fix label association

--- src/shared/components/Input/Switch/Switch.tsx
+++ src/shared/components/Input/Switch/Switch.tsx
@@ -8,13 +8,8 @@ export function Switch({ label, ...props }: SwitchProps) {
   return (
     <div className="flex items-center gap-2">
       <span className="text-lg leading-6 font-medium">{label}</span>
-      <label className="checkbox-container" htmlFor="switch">
-        <input
-          {...props}
-          id="switch"
-          type="checkbox"
-          className="peer sr-only"
-        />
+      <label className="checkbox-container" htmlFor={label}>
+        <input {...props} id={label} type="checkbox" className="peer sr-only" />
         <div className="checkbox-button peer peer-checked:bg-teal-700 peer-checked:after:translate-x-full peer-checked:after:border-teal-700 peer-focus:ring-green-800" />
       </label>
     </div>
