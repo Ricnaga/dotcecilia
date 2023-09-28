8d8f71f27084f70da82b0b2fac1b3fec4d462f9c
Ricnaga <ric.nagato@gmail.com>
2023-09-28 10:57:02 -0300

    refactor(navbar): organize navbars

--- src/shared/components/@dotcecilia/Navbar/Navbar.tsx
+++ src/shared/components/@dotcecilia/Navbar/Navbar.tsx
@@ -1,4 +1,5 @@
 import { Bars3Icon } from '@heroicons/react/20/solid';
+import { useMemo } from 'react';
 import { Button } from '../../Button/Button';
 import { Drawer } from '../../Drawer/Drawer';
 import { IconButton } from '../../IconButton/IconButton';
@@ -10,8 +11,8 @@ export function Navbar() {
     functions: { logOut, setMenuAsOpen },
   } = useNavbar();
 
-  return (
-    <div>
+  const desktopNavbar = useMemo(
+    () => (
       <div className="hidden sm:flex items-center">
         <nav className="grow">
           {links.map(({ href, children }) => (
@@ -28,32 +29,48 @@ export function Navbar() {
           Sair
         </Button>
       </div>
-      <div className="sm:hidden flex justify-end">
-        <IconButton onClick={() => setMenuAsOpen((state) => !state)}>
-          <Bars3Icon width={24} />
-        </IconButton>
-      </div>
-      <Drawer
-        isOpen={menuIsOpen}
-        onClose={() => setMenuAsOpen((state) => !state)}
-      >
-        <div className="h-full w-full">
-          <nav className="flex flex-col gap-10 mt-20 justify-between h-full">
-            {links.map(({ href, children }) => (
-              <a
-                key={href}
-                href={href}
-                className="text-center text-5xl leading-4 font-medium mr-4 hover:text-sky-950 hover:drop-shadow-lg transition-all mb-10"
-              >
-                {children}
-              </a>
-            ))}
-          </nav>
-          <Button variants="center" className="w-full mt-10" onClick={logOut}>
-            Sair
-          </Button>
+    ),
+    [],
+  );
+
+  const mobileNavbar = useMemo(
+    () => (
+      <>
+        <div className="sm:hidden flex justify-end">
+          <IconButton onClick={() => setMenuAsOpen((state) => !state)}>
+            <Bars3Icon width={24} />
+          </IconButton>
         </div>
-      </Drawer>
+        <Drawer
+          isOpen={menuIsOpen}
+          onClose={() => setMenuAsOpen((state) => !state)}
+        >
+          <div className="h-full w-full">
+            <nav className="flex flex-col gap-10 mt-20 justify-between h-full">
+              {links.map(({ href, children }) => (
+                <a
+                  key={href}
+                  href={href}
+                  className="text-center text-5xl leading-4 font-medium mr-4 hover:text-sky-950 hover:drop-shadow-lg transition-all mb-10"
+                >
+                  {children}
+                </a>
+              ))}
+            </nav>
+            <Button variants="center" className="w-full mt-10" onClick={logOut}>
+              Sair
+            </Button>
+          </div>
+        </Drawer>
+      </>
+    ),
+    [menuIsOpen],
+  );
+
+  return (
+    <div>
+      {desktopNavbar}
+      {mobileNavbar}
     </div>
   );
 }
